71d2bf923fa1ea21083249aceff0b7af71a499f1
David Jørgensen <david@pnd.se>
2015-09-21 11:53:00 +0200

    After psd from RC

--- geisha.js
+++ geisha.js
@@ -5,7 +5,8 @@
 //   _|  _|    _|_|_|_|  _|    _|  _|    _|      _|        _|    _|  _|      _|  
 //   _|    _|  _|    _|  _|_|_|    _|    _|  _|    _|_|_|    _|_|    _|      _|  
 //                                                                               
-//   Geisha Counter v1.1                                                                           
+//                                                                               
+//   Geisha Counter v1.2                                                                           
 
 // DIRECTIONS	1. Name id1 and id2 to your html elements
 //				2. Include this file at the bottom of the page
@@ -21,18 +22,43 @@
 			document.getElementById(id).innerHTML = data;				
 	}
 
+	function format(string){
+		var index = string.length - 3,
+			insertable = ' ';
+
+		if(string.length < 3){
+			return string;
+		}
+
+		return string.substring(0, index) + insertable + string.substring(index, string.length);
+	}
+
+	function afterOctober(){
+		set(id1, '500 000')
+		set(id2, '250 000 kr-')
+	}
+
 	function calc(){
 		var month = (new Date()).getMonth();
 		var start = new Date(2015, month, 1);
-		var end = new Date(2015, month+1, 0);
+		var end = new Date(2015, month+1, 1);
+
+		// If after october
+		if(month > 8){
+			afterOctober();
+			return;
+		}
 
 		var now = new Date();
 
 		var fraction = 500000 / (end.getTime() - start.getTime());
 		var sum = fraction * (now.getTime()-start.getTime());
 
-		set(id1, Math.floor(sum))
-		set(id2, Math.floor(sum / 2))
+		var prodStr = format(Math.floor(sum) + '');
+		var sumStr = format(Math.floor(sum / 2) + '');
+
+		set(id1, prodStr)
+		set(id2, sumStr + ' kr-')
 	}
 
 	if(alwaysOn || (new Date()).getMonth() >= 10-1){
